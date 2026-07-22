b407f756c3d1791b16e0f16c26e58cd326f44c09
Steve Meyer <stephen.meyer@wisc.edu>
2026-07-22 10:31:20 -0500

    Moved test code to use pytest

--- test_code.py
+++ test_code.py
@@ -1,3 +1,4 @@
+import pytest
 from eva_data_analysis import text_to_duration
 
 def test_text_to_duration_integer():
@@ -7,9 +8,4 @@ def test_text_to_duration_float():
     assert text_to_duration("10:15") == 10.25
 
 def test_text_to_duration_irrational():
-    assert abs(text_to_duration("10:20") - 10.333333) < 1e-5
-
-
-test_text_to_duration_integer()
-test_text_to_duration_float()
-test_text_to_duration_irrational()
+    assert text_to_duration("10:20") == pytest.approx(10.333333)
